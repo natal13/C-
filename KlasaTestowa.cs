d9f3cf629c957a1224a4dd7256a1ea5ff1ed99fe
Natalia Gadocha <61628135+natal13@users.noreply.github.com>
2021-04-27 13:11:53 +0200

    Add files via upload

---
+++ KlasaTestowa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zajecia2
+{
+    class KlasaTestowa: NowaKlasa
+    {
+        public void metodka2()
+        {
+            Console.WriteLine("Korzystamy z metod z klasy NowaKlasa");
+            metodka(); 
+        }
+       
+
+    }
+}
