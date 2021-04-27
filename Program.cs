de5f213d3e775ffdfaa39f30aac7d5902a59daee
Natalia Gadocha <61628135+natal13@users.noreply.github.com>
2021-04-27 13:14:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dzień dobry użytkowniku!\r\n");
+
+            // TABLICA KROTEK
+            int R;
+            Console.WriteLine("Podaj rozmiar tablicy: ");
+            R = int.Parse(Console.ReadLine());
+
+            var tab_k = new (string nazwisko_k, string imie_k, int nr_id_k)[R];
+
+
+            //INICJALIZATOR 
+            Osoba czlowiek = new Osoba
+            {
+                nazwisko = "Nowak",
+                imie = "Julian",
+                ulica = "Mickiewicza",
+                numer = 15,
+                kod = "30-053",
+                miasto = "Kraków"
+            };
+            
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
